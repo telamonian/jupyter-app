eb1a20c3f703dab7a8237ecbff49834d91869f3a
telamonian <mklein@jhu.edu>
2019-06-13 07:10:00 -0400

    fixed chromium download script
    should be more robust from here on out

--- script/download/chromium.sh
+++ script/download/chromium.sh
@@ -13,11 +13,17 @@ fi
 
 printf "downloading chromium...\n"
 
-# figure out version of latest chromium dev binary
-VERSION=$(curl https://raw.githubusercontent.com/ungoogled-software/ungoogled-chromium-binaries/master/feed.xml  2>/dev/null | grep "<title.*macOS.*development.*>" | sed -e "s/.*macOS: \([-a-zA-Z0-9\.]*\).*/\1/g")
+# get the feed with the info on the latest version
+FEED=$(curl https://raw.githubusercontent.com/ungoogled-software/ungoogled-chromium-binaries/master/feed.xml 2>/dev/null | cat)
 
-# form the actual download url
-URL="https://github.com/unpredictable/ungoogled-chromium-binaries/releases/download/${VERSION}.1/ungoogled-chromium_${VERSION}.1_macos.dmg"
+# figure out version of latest chromium dev binary
+VERSION=$(echo "${FEED}" | grep "<title.*macOS.*development.*>" | sed -e "s/.*macOS: \([-a-zA-Z0-9\.]*\).*/\1/g")
+
+# get the link to the download page
+DOWNLOAD_PAGE_URL=$(echo "${FEED}" | grep "<link href.*macos/${VERSION}.*>" | sed -e "s/.*<link href=\"\(http.*\)\" \/>/\1/g")
+
+# fetch the actual download url
+URL=$(curl "${DOWNLOAD_PAGE_URL}" 2>/dev/null | grep macos.dmg | sed -e "s/.*<a href=\"\(http.*macos.dmg\)\">.*/\1/g")
 
 # do the actual download. -L to follow redirects
 dryecho curl -L $URL -o ${dmgloc}
