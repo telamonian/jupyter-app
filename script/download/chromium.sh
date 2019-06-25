c24a85ac4451b69abcdf21e05267125c08e5006e
telamonian <mklein@jhu.edu>
2019-06-25 04:07:55 -0400

    cleaned up install from zip

--- script/download/chromium.sh
+++ script/download/chromium.sh
@@ -13,11 +13,16 @@ fi
 
 printf "downloading chromium...\n"
 
-# figure out version of latest chromium dev binary
-VERSION=$(curl https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media 2>/dev/null)
+# this download protocol is based on Homebrew Cask's chromium formula
 
-# form the actual download url
-URL="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/${VERSION}/chrome-mac.zip"
+## figure out version of latest chromium dev binary
+#VERSION=$(curl https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media 2>/dev/null)
+#
+## form the actual download url
+#URL="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/${VERSION}/chrome-mac.zip"
+
+# download url from https://download-chromium.appspot.com/
+URL="https://download-chromium.appspot.com/dl/Mac?type=snapshots"
 
 # download the zip. -L to follow redirects
 dryecho curl -L $URL -o "${zipPath}"
